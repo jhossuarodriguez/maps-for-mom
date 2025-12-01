69c302cd355461d88ce229ee653d4efc0ed30c29
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-12-01 07:58:03 -0400

    Adding existing files to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
+import path from 'node:path'
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  output: 'server',
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  },
+
+  integrations: [sitemap()],
+});
